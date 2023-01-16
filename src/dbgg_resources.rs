7d192cafb294afd1445ef7788728b18de048744b
Alexandre Vanini <vanini.alexandre@gmail.com>
2023-01-16 19:40:41 +0100

    Add Data struct and add groups to tasks

--- src/dbgg_resources.rs
+++ src/dbgg_resources.rs
@@ -4,22 +4,21 @@ use axum::{extract::Path, Json};
 
 use crate::errors::AppError;
 
+#[derive(Debug)]
 pub struct Member {
     pub name: &'static str,
     pub id: i32,
 }
 
-pub(crate) async fn get_members() -> Result<Json<Vec<Member>>, AppError> {
-    Ok(Json(
-        vec!["Vanini", "Gamerdinger", "Henriette", "Jon"]
-            .iter()
-            .enumerate()
-            .map(|(id, name)| Member {
-                name,
-                id: id as i32,
-            })
-            .collect(),
-    ))
+fn get_members() -> Vec<Member> {
+    vec!["Vanini", "Gamerdinger", "Henriette", "Jon"]
+        .iter()
+        .enumerate()
+        .map(|(id, name)| Member {
+            name,
+            id: id as i32,
+        })
+        .collect()
 }
 
 // pub(crate) async fn task(Path(member): Path<String>) -> Result<Json<Vec<Member>>, AppError> {
@@ -41,18 +40,51 @@ pub struct Task {
     pub name: &'static str,
 }
 
+#[derive(Debug)]
+pub struct CollectiveInformation {
+    pub tasks: HashMap<i32, Vec<Task>>,
+    pub number_of_task: i32,
+    pub total_complexity: i32,
+    pub members: Vec<Member>,
+    pub num_members: i32,
+    pub max_complexity_per_member: i32,
+}
+
 // unsure yet how to store that
-pub(crate) fn get_tasks() -> HashMap<i32, Vec<Task>> {
+pub(crate) fn get_tasks_for_member() -> Result<(), AppError> {
+    let data = get_colletive_information();
+
+    Ok(())
+}
+
+// unsure yet how to store that
+fn get_colletive_information() -> CollectiveInformation {
     let mut tasks: HashMap<i32, Vec<Task>> = HashMap::new();
-    
-    for (area, name, priority) in vec![
+    let mut number_of_task = 0;
+    let mut total_complexity = 0;
+
+    for (area, name, difficulty) in vec![
         (Area::Bathroom, "Clean mirror", 1),
         (Area::Bathroom, "Clean sink + tap", 1),
         (Area::Bathroom, "Clean shower (Floor - Shower head)", 3),
+        (Area::Bathroom, "Clean toilet", 3),
     ] {
         let task = Task { area, name };
-        tasks.entry(priority).or_default().push(task);
+        tasks.entry(difficulty).or_default().push(task);
+
+        number_of_task += 1;
+        total_complexity += difficulty;
     }
 
-    tasks
+    let members = get_members();
+    let num_members = members.len() as i32;
+    
+    CollectiveInformation {
+        tasks,
+        number_of_task,
+        total_complexity,
+        members,
+        num_members,
+        max_complexity_per_member: total_complexity / num_members,
+    }
 }
